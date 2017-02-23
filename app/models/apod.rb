c616267506a360a1fdbbd038582e8da42bdb9dfb
Ilaria Varriale <ilariasvarriale@gmail.com>
2017-02-23 13:25:03 -0800

    Adds video media type conditional to view

--- app/models/apod.rb
+++ app/models/apod.rb
@@ -1,6 +1,6 @@
 class Apod < ApplicationRecord
-  def get_apod_info
-    apod_url = "https://api.nasa.gov/planetary/apod?api_key=#{ENV['NASA_API_KEY']}"
+  def self.get_apod_info
+    apod_url = "https://api.nasa.gov/planetary/apod?api_key=#{ENV["NASA_APOD_API_KEY"]}"
     response = Net::HTTP.get(URI(apod_url))
     JSON.parse(response)
   end
